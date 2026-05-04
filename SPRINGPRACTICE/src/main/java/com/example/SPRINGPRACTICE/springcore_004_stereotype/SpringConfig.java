63e229c05c825d59417cd790304344d391c9f178
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-04 18:51:27 +0530

    spring config

---
+++ SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/springcore_004_stereotype/SpringConfig.java
@@ -0,0 +1,15 @@
+package com.example.SPRINGPRACTICE.springcore_004_stereotype;
+
+
+
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.itshaala"})
+public class SpringConfig {
+
+
+
+}
