02b1f7d91daa1ba48ee971fd6af3b5f85d5551e4
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-05 21:31:11 +0530

    spring

---
+++ SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/spring_core_005_propertysource/SpringConfig.java
@@ -0,0 +1,12 @@
+package com.example.SPRINGPRACTICE.spring_core_005_propertysource;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.itshaala")
+public class SpringConfig {
+
+
+}
