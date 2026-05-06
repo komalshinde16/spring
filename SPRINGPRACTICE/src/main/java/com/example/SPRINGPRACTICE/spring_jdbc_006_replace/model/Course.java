f4cdfbbf492fffa858018b29e326bcf2399daf95
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-06 18:32:32 +0530

    course

---
+++ SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/spring_jdbc_006_replace/model/Course.java
@@ -0,0 +1,12 @@
+package com.example.SPRINGPRACTICE.spring_jdbc_006_replace.model;
+
+public class Course {
+
+
+    private int id;
+    private String name;
+    private String duration;
+    private int price;
+    private String description;
+
+}
