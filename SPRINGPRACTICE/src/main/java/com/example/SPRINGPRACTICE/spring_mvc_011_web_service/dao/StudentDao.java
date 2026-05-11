354ffc31970feef2c1191a3d2346adee3b710cb1
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-11 10:50:08 +0530

    dao & hibernate

--- SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/spring_mvc_011_web_service/dao/StudentDao.java
+++ SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/spring_mvc_011_web_service/dao/StudentDao.java
@@ -1,4 +1,54 @@
 package com.example.SPRINGPRACTICE.spring_mvc_011_web_service.dao;
 
+import com.example.SPRINGPRACTICE.spring_mvc_011_web_service.model.Student;
+import com.example.SPRINGPRACTICE.spring_mvc_011_web_service.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
 public class StudentDao {
+
+    public void addStudent(Student student) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.persist(student);
+        tx.commit();
+        session.close();
+    }
+
+    public void updateStudent(Student student) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.merge(student);
+        tx.commit();
+        session.close();
+    }
+
+    public void deleteStudent(int id) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.remove(id);
+        tx.commit();
+        session.close();
+    }
+
+    public Student getStudent(int id) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Student student = session.find(Student.class, id);
+        return student;
+    }
+
+    public List<Student> getAllStudents() {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        List list = session.createQuery("from Student ").list();
+        session.close();
+        return list;
+    }
 }
--- SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/spring_mvc_011_web_service/util/HibernateUtil.java
+++ SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/spring_mvc_011_web_service/util/HibernateUtil.java
@@ -1,4 +1,18 @@
 package com.example.SPRINGPRACTICE.spring_mvc_011_web_service.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory;
+    }
 }
