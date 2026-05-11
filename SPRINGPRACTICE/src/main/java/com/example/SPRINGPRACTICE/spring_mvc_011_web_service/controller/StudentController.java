30dbef0378507c2ed4c8959d3197b63dc46e13c7
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-11 10:56:31 +0530

    controller and service

--- SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/spring_mvc_011_web_service/controller/StudentController.java
+++ SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/spring_mvc_011_web_service/controller/StudentController.java
@@ -1,4 +1,50 @@
 package com.example.SPRINGPRACTICE.spring_mvc_011_web_service.controller;
 
+
+import com.example.SPRINGPRACTICE.spring_mvc_011_web_service.model.Student;
+import com.example.SPRINGPRACTICE.spring_mvc_011_web_service.service.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+@AllArgsConstructor
+
+
 public class StudentController {
+
+
+    private StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Student> addStudent(@RequestBody Student student) {
+        studentService.addStudent(student);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student) {
+        studentService.updateStudent(student);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable("id") int id) {
+        studentService.deleteStudent(id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable("id") int id) {
+        return new ResponseEntity<>(studentService.getStudent(id), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return new ResponseEntity<>(studentService.getAllStudents(), HttpStatus.OK);
+    }
+
 }
--- SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/spring_mvc_011_web_service/service/StudentService.java
+++ SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/spring_mvc_011_web_service/service/StudentService.java
@@ -1,4 +1,36 @@
 package com.example.SPRINGPRACTICE.spring_mvc_011_web_service.service;
 
+
+import com.example.SPRINGPRACTICE.spring_mvc_011_web_service.dao.StudentDao;
+import com.example.SPRINGPRACTICE.spring_mvc_011_web_service.model.Student;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class StudentService {
+
+    private StudentDao studentDao;
+
+    public void addStudent(Student student) {
+        studentDao.addStudent(student);
+    }
+
+    public void updateStudent(Student student) {
+        studentDao.updateStudent(student);
+    }
+
+    public void deleteStudent(int id) {
+        studentDao.deleteStudent(id);
+    }
+
+    public Student getStudent(int id) {
+        return studentDao.getStudent(id);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentDao.getAllStudents();
+    }
 }
