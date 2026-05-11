e1c24fc24a94f236dd79cee92ed3b8cf6a2684d6
komalshinde16 <komalambadasshinde@gmail.com>
2026-05-11 10:45:52 +0530

    student

--- SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/spring_mvc_011_web_service/model/Student.java
+++ SPRINGPRACTICE/src/main/java/com/example/SPRINGPRACTICE/spring_mvc_011_web_service/model/Student.java
@@ -1,4 +1,21 @@
 package com.example.SPRINGPRACTICE.spring_mvc_011_web_service.model;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "student")
+
+
+
+
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private int id;
+    private int age;
+    private String name;
+    private String address;
+
 }
